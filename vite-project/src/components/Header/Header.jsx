9676f5cffbf548ca346a0268c6c5103892f36b31
Татьяна Пудова <tapotusha@icloud.com>
2025-11-12 18:11:01 +0300

    added some things

---
+++ vite-project/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return(
+        <header>
+            <div>
+                <nav>
+                    <NavLink to="/">Главная</NavLink>
+                </nav>
+
+            </div>
+        </header>
+    );
+}
---
+++ vite-project/src/layout/MainLayout/MainLayout.jsx
@@ -0,0 +1,7 @@
+export default function MainLayout(){
+    return(
+        <div>
+            
+        </div>
+    );
+}
